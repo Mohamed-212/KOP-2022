c505e09a9b77bdedbc9e3f248c161ceece2ac8f9
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-04 13:50:04 +0100

    set app.js

--- public/js/app.js
+++ public/js/app.js
@@ -49325,7 +49325,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nTypeError: Cannot read property 'parseComponent' of undefined\n    at parse (C:\\xampp\\htdocs\\Kopy\\node_modules\\@vue\\component-compiler-utils\\dist\\parse.js:14:23)\n    at Object.module.exports (C:\\xampp\\htdocs\\Kopy\\node_modules\\vue-loader\\lib\\index.js:67:22)");
+throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.js):\nTypeError: Cannot read properties of undefined (reading 'parseComponent')\n    at parse (C:\\laragon\\www\\kop\\node_modules\\@vue\\component-compiler-utils\\dist\\parse.js:14:23)\n    at Object.module.exports (C:\\laragon\\www\\kop\\node_modules\\vue-loader\\lib\\index.js:67:22)");
 
 /***/ }),
 
@@ -49347,8 +49347,8 @@ throw new Error("Module build failed (from ./node_modules/vue-loader/lib/index.j
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\Kopy\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Kopy\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\kop\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\kop\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
