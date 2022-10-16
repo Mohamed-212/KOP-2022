283e7059de82ab1339f948a9747a05823e7a9331
abo3adel <ahmed.adel@212solutionsllc.com>
2022-10-16 13:51:07 +0100

    fix get order items seperated

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -251,38 +251,38 @@ class OrdersController extends BaseController
             $role->where('name', 'cashier');})->get();
         if ($cashiers) {
             foreach ($cashiers as $cashier) {
-              \App\Http\Controllers\NotificationController::pushNotifications($cashier->user_id, "New Order has been placed", "Order", null, null, $request->customer_id);
+            //   \App\Http\Controllers\NotificationController::pushNotifications($cashier->user_id, "New Order has been placed", "Order", null, null, $request->customer_id);
             }
         }
 
         // $subtotal = 0;
 
-        $items = [];
+        $items = $request->items;
         // dd($request->items);
-        foreach ($request->items as $item) {
-            if (gettype($item) == 'object') {
-                $item = $item->toArray();
-            }
-            // if ($item['quantity'] > 1) {
-            for ($i = 0; $i < $item['quantity']; $i++) {
-                $items[] = [
-                    'item_id' => $item['item_id'],
-                    'quantity' => 1,
-                    'offer_price' => isset($item['offer_price']) ? $item['offer_price'] : null,
-                    'price' => $item['price'],
-                    'offerId' => isset($item['offerId']) ? $item['offerId'] : null,
-                    'extras' => isset($item['extras'][$i]) && count($item['extras']) ? $item['extras'][$i] : [],
-                    'withouts' => isset($item['withouts'][$i]) && count($item['withouts']) ? $item['withouts'][$i] : [],
-                    'dough_type_ar' => isset($item['dough_type_ar'][$i]) ? $item['dough_type_ar'][$i] : null,
-                    'dough_type_en' => isset($item['dough_type_en'][$i]) ? $item['dough_type_en'][$i] : null,
-                    'dough_type_2_ar' => isset($item['dough_type_2_ar'][$i]) ? $item['dough_type_2_ar'][$i] : null,
-                    'dough_type_2_en' => isset($item['dough_type_2_en'][$i]) ? $item['dough_type_2_en'][$i] : null,
-                ];
-            }
-            // } else {
-            //     $items[] = $item;
-            // }  
-        }
+        // foreach ($request->items as $item) {
+        //     if (gettype($item) == 'object') {
+        //         $item = $item->toArray();
+        //     }
+        //     // if ($item['quantity'] > 1) {
+        //     for ($i = 0; $i < $item['quantity']; $i++) {
+        //         $items[] = [
+        //             'item_id' => $item['item_id'],
+        //             'quantity' => 1,
+        //             'offer_price' => isset($item['offer_price']) ? $item['offer_price'] : null,
+        //             'price' => $item['price'],
+        //             'offerId' => isset($item['offerId']) ? $item['offerId'] : null,
+        //             'extras' => isset($item['extras'][$i]) && count($item['extras']) ? $item['extras'][$i] : [],
+        //             'withouts' => isset($item['withouts'][$i]) && count($item['withouts']) ? $item['withouts'][$i] : [],
+        //             'dough_type_ar' => isset($item['dough_type_ar'][$i]) ? $item['dough_type_ar'][$i] : null,
+        //             'dough_type_en' => isset($item['dough_type_en'][$i]) ? $item['dough_type_en'][$i] : null,
+        //             'dough_type_2_ar' => isset($item['dough_type_2_ar'][$i]) ? $item['dough_type_2_ar'][$i] : null,
+        //             'dough_type_2_en' => isset($item['dough_type_2_en'][$i]) ? $item['dough_type_2_en'][$i] : null,
+        //         ];
+        //     }
+        //     // } else {
+        //     //     $items[] = $item;
+        //     // }  
+        // }
 
         foreach ($items as $item) {
 
