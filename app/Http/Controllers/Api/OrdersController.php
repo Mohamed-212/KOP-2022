d85d653adc26d7fd38865836818fdbf631522e8d
abo3adel <ahmed.adel@212solutionsllc.com>
2022-11-07 14:53:12 +0000

    fix offer price not returning on get user order

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -78,6 +78,7 @@ class OrdersController extends BaseController
             foreach ($order->items as $item) {
                 $item->extras = Extra::whereIn('id', explode(', ', $item->pivot->item_extras))->get();
                 $item->withouts = Without::whereIn('id', explode(', ', $item->pivot->item_withouts))->get();
+                $item->offer_price = $item->pivot->offer_price;
                 // $extras = $item->pivot->item_extras;
                 // $extras = $extras ? explode(", ", $extras) : [];
 
